946cb69cad974c5427d6d13c37f4bb7c621435a0
unknown <pantm1004@gmail.com>
2026-01-10 21:51:10 +0530

    create a toggle button

--- script.js
+++ script.js
@@ -937,3 +937,18 @@ let para = document.querySelector("#text");
 btn3.addEventListener("click", () => {
   para.innerText = "Text Changed!";
 });
+
+//create a toggle button that changes the screen to dark-mode when clicked & light-mode when clicked again.
+let modeBtn = document.querySelector("#mode");
+let currMode =  "light"; 
+
+modeBtn.addEventListener("click", () => {
+if(currMode === "light"){
+  currMode = "dark";
+  document.querySelector("body").style.backgroundColor = "black";
+}else{
+  currMode = "light";
+  document.querySelector("body").style.backgroundColor = "white";
+}
+console.log(currMode);
+});
